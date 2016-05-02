a2abfbea572953a67bec87081775354fdaa9c1c2
Chris Calderon <calderon.christian760@gmail.com>
2016-05-01 22:17:34 -0700

    Updated test.py to use my SerpentTests module + added test_requirements.txt for this, added .gitignore

--- test.py
+++ test.py
@@ -1,54 +1,24 @@
-import warnings; warnings.simplefilter('ignore')
-from ethereum import tester as t
-from ethereum import utils
-import sys
-from colorama import Fore, Style, init; init()
+from serpent_tests import Tester
 
-passed = Fore.GREEN + 'passed' + Style.RESET_ALL
-failed = Style.BRIGHT + Fore.RED + 'failed' + Style.RESET_ALL
+def main():
+    myTest = Tester('liststore.se')
+    owner = myTest.accounts[0].address_as_int
+    vals = range(1, 11)
+    tests = (('addVal', (1, 1337), {}, 1),
+             ('getList', (owner, 1), {}, [1337]),
+             ('getMyList', (1,), {}, [1337]),
+             ('addVals', (1, vals), {}, 1),
+             ('getMyList', (1,), {}, [1337] + vals),
+             ('isMember', (owner, 1, 1337), {}, 1),
+             ('isMember', (owner, 1, 11), {}, 0),
+             ('remove', (1, 1337), {}, 1),
+             ('remove', (1, 1337), {}, 0),
+             ('getList', (owner, 1), {}, [0] + vals),
+             ('replace', (1, 5, 1337), {}, 1),
+             ('getMyList', (1,), {}, [0] + [1,2,3,4,1337,6,7,8,9,10]),
+             ('addVal', (1, 27), {}, 1),
+             ('getMyList', (1,), {}, [27,1,2,3,4,1337,6,7,8,9,10]))
+    myTest.run_tests(tests)
 
-def test(func, args, expected):
-    sys.stdout.write('testing {}: '.format(func.__name__))
-    sys.stdout.flush()
-    result = func(*args)
-    if result == expected:
-        print passed
-    else:
-        arg_str = ', '.join(map(str, args))
-        print failed
-        print '  {}({}) => {}'.format(func.__name__, arg_str, result)
-        print '  expected {}'.format(expected)
-        sys.exit(1)
-
-def abi_to_int(s):
-    if s == '':
-        return 0
-    return abi_to_int(s[:-1])<<8 | ord(s[-1])
-
-s = t.state()
-c = s.abi_contract('liststore.se')
-owner = abi_to_int(t.a0)
-vals = range(1,11)
-
-for name, thing in vars(c).items():
-    if getattr(thing, '__name__', '') == 'kall':
-        thing.__name__ = str(name)
-
-tests = ((c.addVal, (1, 1337), 1),
-         (c.getList, (owner, 1), [1337]),
-         (c.getMyList, (1,), [1337]),
-         (c.addVals, (1, vals), 1),
-         (c.getMyList, (1,), [1337] + vals),
-         (c.isMember, (owner, 1, 1337), 1),
-         (c.isMember, (owner, 1, 11), 0),
-         (c.remove, (1, 1337), 1),
-         (c.remove, (1, 1337), 0),
-         (c.getList, (owner, 1), [0] + vals),
-         (c.replace, (1, 5, 1337), 1),
-         (c.getMyList, (1,), [0] + [1,2,3,4,1337,6,7,8,9,10]),
-         (c.addVal, (1, 27), 1),
-         (c.getMyList, (1,), [27,1,2,3,4,1337,6,7,8,9,10]))
-
-for func, args, exp in tests:
-    test(func, args, exp)
-    s.mine(1)
+if __name__ == '__main__':
+    main()
